625a9d0c6c6c5beba36c43228d5feea56f2ce94f
JohnBainto <john_matthew_bainto@dlsu.edu.ph>
2021-08-25 04:29:48 +0800

    Added options menu with search and sort options.

--- app/src/main/java/com/mobdeve/s16/bainto/john/freshnesscheck/MainActivity.java
+++ app/src/main/java/com/mobdeve/s16/bainto/john/freshnesscheck/MainActivity.java
@@ -2,7 +2,10 @@ package com.mobdeve.s16.bainto.john.freshnesscheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.sortName) {
+
+        }
+        else if (id == R.id.sortExpiration) {
+
+        }
+
+        return true;
     }
 }
