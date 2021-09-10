7e3943d748e44fcd7a06f9af23ffa7fc3206a14b
JohnBainto <john_matthew_bainto@dlsu.edu.ph>
2021-09-10 19:09:40 +0800

    Added DatePickerDialog for selecting item expiration date.

--- app/src/main/java/com/mobdeve/s16/bainto/john/freshnesscheck/AddItemActivity.java
+++ app/src/main/java/com/mobdeve/s16/bainto/john/freshnesscheck/AddItemActivity.java
@@ -1,9 +1,11 @@
 package com.mobdeve.s16.bainto.john.freshnesscheck;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -11,11 +13,18 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class AddItemActivity extends AppCompatActivity {
     public static final String
             NEW_ITEM_NAME_KEY = "NEW_ITEM_NAME_KEY",
             NEW_ITEM_CATEGORY_KEY = "NEW_ITEM_CATEGORY_KEY",
-            NEW_ITEM_EXPIRATION_KEY = "NEW_ITEM_EXPIRATION_KEY";
+            NEW_ITEM_EXPIRATION_KEY = "NEW_ITEM_EXPIRATION_KEY",
+            ADD_ITEM_TAG = "AddItemActivity";
+
+    private Calendar myCalendar = new GregorianCalendar();
 
     private TextView addItemNameTv, addItemCategoryTv;
     private EditText addItemExpirationEt;
@@ -26,10 +35,19 @@ public class AddItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        this.addItemNameTv = findViewById(R.id.add_item_name_tv);
-        this.addItemCategoryTv = findViewById(R.id.add_item_category_tv);
-        this.addItemExpirationEt = findViewById(R.id.add_item_date_date);
+        this.addItemNameTv = findViewById(R.id.addItemNameEt);
+        this.addItemCategoryTv = findViewById(R.id.addItemCategoryEt);
+        this.addItemExpirationEt = findViewById(R.id.addItemDateEt);
         this.addBtn = findViewById(R.id.add_item_add_btn);
+
+        addItemExpirationEt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddItemActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         this.addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +71,22 @@ public class AddItemActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateDate();
+        }
+    };
+
+    private void updateDate() {
+        String myFormat = "dd/MM/yy"; //put your date format in which you need to display
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        addItemExpirationEt.setText(sdf.format(myCalendar.getTime()));
     }
 }
