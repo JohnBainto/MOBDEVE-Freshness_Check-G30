7ae15071616f632c5595a032d571f06b571b031f
JohnBainto <john_matthew_bainto@dlsu.edu.ph>
2021-09-15 23:19:12 +0800

    Implemented receiving Intent and making changes on passed Item object when editing an existing item.

--- app/src/main/java/com/mobdeve/s16/bainto/john/freshnesscheck/AddItemActivity.java
+++ app/src/main/java/com/mobdeve/s16/bainto/john/freshnesscheck/AddItemActivity.java
@@ -16,32 +16,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AddItemActivity extends AppCompatActivity {
     public static final String
             NEW_ITEM_NAME_KEY = "NEW_ITEM_NAME_KEY",
             NEW_ITEM_CATEGORY_KEY = "NEW_ITEM_CATEGORY_KEY",
             NEW_ITEM_EXPIRATION_KEY = "NEW_ITEM_EXPIRATION_KEY",
-            ADD_ITEM_TAG = "AddItemActivity",
+            EDIT_ITEM_TAG = "EDIT_ITEM",
             NEW_ITEM_YEAR_KEY = "NEW+ITEM_YEAR_KEY",
             NEW_ITEM_MONTH_KEY = "NEW+NEW_ITEM_MONTH_KEY",
             NEW_ITEM_DAY_KEY = "NEW+NEW_ITEM_DAY_KEY";
 
     private Calendar myCalendar = new GregorianCalendar();
-
+    private char type;
+    private DbHelper dbHelper;
     private TextView addItemNameTv, addItemCategoryTv;
     private EditText addItemExpirationEt;
     private ImageButton addBtn;
+    private Intent intent;
+    private Item item;
+
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        this.addItemNameTv = findViewById(R.id.addItemNameEt);
-        this.addItemCategoryTv = findViewById(R.id.addItemCategoryEt);
-        this.addItemExpirationEt = findViewById(R.id.addItemDateEt);
-        this.addBtn = findViewById(R.id.add_item_add_btn);
+        addItemNameTv = findViewById(R.id.addItemNameEt);
+        addItemCategoryTv = findViewById(R.id.addItemCategoryEt);
+        addItemExpirationEt = findViewById(R.id.addItemDateEt);
+        addBtn = findViewById(R.id.add_item_add_btn);
+
+        intent = getIntent();
+        type = intent.getCharExtra(ItemsActivity.ITEM_KEY_TYPE, 'a');
+
+        if(type == 'e') {
+            item = intent.getParcelableExtra(ItemsActivity.ITEM_KEY);
+
+            addItemNameTv.setText(item.getName());
+            addItemCategoryTv.setText(item.getCategory());
+            addItemExpirationEt.setText(item.getDate());
+        }
 
         addItemExpirationEt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,7 +73,7 @@ public class AddItemActivity extends AppCompatActivity {
         this.addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!addItemNameTv.getText().toString().isEmpty() && !addItemCategoryTv.getText().toString().isEmpty() && !addItemExpirationEt.getText().toString().isEmpty()) {
+                if(!addItemNameTv.getText().toString().isEmpty() && !addItemCategoryTv.getText().toString().isEmpty() && !addItemExpirationEt.getText().toString().isEmpty() && type == 'a') {
                     Intent intent = new Intent();
                     intent.putExtra(NEW_ITEM_NAME_KEY, addItemNameTv.getText().toString());
                     intent.putExtra(NEW_ITEM_CATEGORY_KEY, addItemCategoryTv.getText().toString());
@@ -67,6 +85,28 @@ public class AddItemActivity extends AppCompatActivity {
                     setResult(Activity.RESULT_OK, intent);
 
                     finish();
+                }
+                else if(!addItemNameTv.getText().toString().isEmpty() && !addItemCategoryTv.getText().toString().isEmpty() && !addItemExpirationEt.getText().toString().isEmpty() && type == 'e') {
+                    executorService.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            dbHelper = dbHelper.getInstance(AddItemActivity.this);
+
+                            dbHelper.updateItemName(addItemNameTv.getText().toString(), item.getId(), item.getName());
+                            dbHelper.updateItemCategory(addItemCategoryTv.getText().toString(), item.getId(), item.getName());
+                            dbHelper.updateItemExpiration(addItemExpirationEt.getText().toString(), item.getId(), item.getName());
+
+                            item.setName(addItemNameTv.getText().toString());
+                            item.setCategory(addItemCategoryTv.getText().toString());
+                            item.setDate(addItemExpirationEt.getText().toString());
+
+                            Intent intent = new Intent();
+                            intent.putExtra(EDIT_ITEM_TAG, item);
+                            setResult(Activity.RESULT_OK, intent);
+
+                            finish();
+                        }
+                    });
                 }
                 else {
                     Toast.makeText(
